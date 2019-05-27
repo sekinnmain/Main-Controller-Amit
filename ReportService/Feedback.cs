15418524181c69351852eb0b510957d568310eec
amitdahari <amitdahary@gmail.com>
2019-05-27 16:53:36 +0300

    ADDED NEW CHANGES

---
+++ ReportService/Feedback.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportService
+{
+    class Feedback
+    {
+        string customerFeedback;
+        int rateFeedback;
+
+        public Feedback(string CutomerFeedback, int RateFeedback)
+        {
+            this.customerFeedback = CutomerFeedback;
+            this.rateFeedback = RateFeedback;
+        }
+
+        
+        public string ChangeYourFeedback(string ChangedCustomerFeedback)
+        {
+            this.customerFeedback = ChangedCustomerFeedback;
+        }
+
+        public int ChangeYourRankedFeedback(int ChangedrateFeedback)
+        {
+           this.rateFeedback = ChangedrateFeedback; 
+        }
+    }
+}
---
+++ ReportService/ReportService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportService
+{
+    namespace ReportService
+    {
+        public class ReportService
+        {
+            public int TotallDayWorked(DateTime date)
+            {
+
+            }
+            public float WorkerWorkedTime(int worker_Id)
+            {
+
+            }
+            public int SoldDishes(DateTime date)
+            {
+
+            }
+        }
+    }
+ }
